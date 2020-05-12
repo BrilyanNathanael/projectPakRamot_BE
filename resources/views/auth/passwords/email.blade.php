d79add3372c03249d28dbbb3e14a0fe14af546a8
BrilyanNathanael <brilyannaathan@gmail.com>
2020-05-12 17:23:45 +0700

    Forgot Password Fixed

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,47 +3,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/forgotPassword.css">
     <title>Forgot Password</title>
+    <link rel="stylesheet" href="/css/forgotPassword.css">
+    <link rel="stylesheet" href="fontawesome/css/all.css">
 </head>
 <body>
-    <div class="box">
-        <h1>{{ __('Reset Password') }}</h1>
-        @if (session('status'))
-            <div class="alert alert-success" role="alert">
-                {{ session('status') }}
+    <div class="container">
+        <div class="box">
+            <h1>{{ __('Reset Password') }}</h1>
+            <div class="form-list">
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
+                        <div class="form-group">
+                            @if (session('status'))
+                                <div class="alert" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+                            <div class="label">
+                                <label for="email">{{ __('Email') }}</label>
+                            </div>
+            
+                            <div class="list">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <p>*We will send you a reset password link to your email,<br>
+                                please enter correct email.</p>
+            
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+            
+                        <div class="send">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Send') }}
+                            </button>          
+                        </div>
+                        
+                </form>
             </div>
-        @endif
+        </div>
 
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-            <div class="emails">
-                <div class="form-group">
-                    <div class="email-label">
-                        <label for="email">{{ __('E-Mail') }}</label>
-                    </div>
-    
-                    <div class="email_list">
-                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                        <p>*We will send you a reset password link to your email,<br>
-                        please enter correct email.</p>
-    
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                </div>
-    
-                <div class="send">
-                    <button type="submit" class="btn btn-primary">
-                        {{ __('Send') }}
-                    </button>          
-                </div>
-            </div>
-
-        </form>
     </div>
 </body>
 </html>
