92416a03131d4832af7716b304f4b7d55d9d5489
BrilyanNathanael <brilyannaathan@gmail.com>
2020-05-17 21:02:35 +0700

    Verify Added

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Forgot Password</title>
+    <title>Verify</title>
     <link rel="stylesheet" href="/css/verify.css">
     <link rel="stylesheet" href="fontawesome/css/all.css">
 </head>
